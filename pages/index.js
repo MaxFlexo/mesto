548a99debe2ae79647236bd46da04b3dff378bf9
Maksim Bubnov <max.flexo@gmail.com>
2021-01-10 23:38:20 +0300

    исправления в css, разметке и js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ let openButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-btn');
 let subButton = popup.querySelector('.popup__sub-button');
+let name = document.querySelector('.popup__input_type_username');
+let job = document.querySelector('.popup__input_type_username-description');
+let nameOnPage = document.querySelector('.profile__name');
+let jobOnPage = document.querySelector('.profile__description');
+let formElement = popup.querySelector('.popup__container');
 
 let openPopup = () => {
   popup.classList.add('popup_opened');
-  let name = document.querySelector('.popup__input_type_username');
-  let job = document.querySelector('.popup__input_type_username-description');
-  let nameOnPage = document.querySelector('.profile__name');
-  let jobOnPage = document.querySelector('.profile__description');
   name.value = nameOnPage.textContent;
   job.value = jobOnPage.textContent;
 }
@@ -23,17 +24,11 @@ closeButton.addEventListener('click', closePopup);
 
 subButton.addEventListener('click', closePopup);
 
-let formElement = popup.querySelector('.popup__container');
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
-    let nameInput = formElement.querySelector('.popup__input_type_username');
-    let jobInput = formElement.querySelector('.popup__input_type_username-description');
-    let nameText = document.querySelector('.profile__name');
-    let jobText = document.querySelector('.profile__description');
-    nameText.textContent = nameInput.value;
-    jobText.textContent = jobInput.value;
+    nameOnPage.textContent = name.value;
+    jobOnPage.textContent = job.value;
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
